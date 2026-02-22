0e7b63dc739864659ac14eb4cd165b7c056f0623
Zhenyu Cai <czy051217@qq.com>
2026-02-22 13:50:42 +0800

    添加了PSRAM 2.1v
    加入PSRAM缓存的example3

--- main/main.cc
+++ main/main.cc
@@ -4,19 +4,24 @@
 #include "freertos/task.h"
 #include "esp_log.h"   
 #include "esp_err.h"  
-
+#include "esp_heap_caps.h"
 /* 显示组件 */
 #include "LCD_gc9a01/my_gc9a01.h"
 #include "lv_port_disp.h"
 #include "lvgl.h"
 #include "lvgl_tick_timer.h"
 #include "lvgl_UI_display.h"
+
+
 static const char *TAG = "main";
 
 
 extern "C" void app_main(void)
 {
     printf("enter app_main\n");
+
+  size_t psram_free = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
+    ESP_LOGI(TAG, "Initial PSRAM free size: %d bytes", psram_free);
 
     ESP_LOGI(TAG, "Initialize LVGL...");
     lv_init();
@@ -33,11 +38,22 @@ extern "C" void app_main(void)
 
 
     ESP_LOGI(TAG, "5. Enter main loop...");
+
+    uint16_t get_PSRAM_count = 0;
     // 运行 LVGL 任务处理循环
     while (1) {
         // 处理 LVGL 的绘制、动画和输入事件
         lv_timer_handler();
+        ESP_LOGI(TAG, "running...");
+        get_PSRAM_count++;
+        if(get_PSRAM_count>100)
+        {
+            psram_free = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
+            ESP_LOGI(TAG, "Current PSRAM free size: %d bytes", psram_free);
+            get_PSRAM_count = 0;
+        }
         
+
         // 释放 CPU 资源，防止触发看门狗 (Watchdog) 报错
         vTaskDelay(pdMS_TO_TICKS(10)); 
     }
