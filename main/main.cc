184ab526f16b1bc4427293e5f4552da62cba3cb1
Zhenyu Cai <czy051217@qq.com>
2026-02-22 23:43:45 +0800

    解码时间还是太长 容易触发看门狗 4.4v
    当gif图片大点，解码时间长就容易触发看门狗了

--- main/main.cc
+++ main/main.cc
@@ -25,7 +25,7 @@
 // ▼▼▼ 调试打印开关：1代表开启，0代表彻底关闭 ▼▼▼
 #define DEBUG_MODE 1
 
-#define GIF_FILE_PATH "on.gif"
+#define GIF_FILE_PATH "dog3.gif"
 
 static const char *TAG = "main";
 static const char *T_TAG = "SYS_MONITOR";
